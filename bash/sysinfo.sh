fd24949699b322fe29a6bed2b77e96ab0a709a5c
Shubham Bhardwaj <shub200213@gmail.com>
2023-06-01 14:55:22 -0400

    Updated sysinfo.sh for Lab-2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 #echo commands are used to display labels
 
-echo "FQDN:"
 #Hostname command with "-f" option to display the fqdn information only
-hostname -f
+fqdn=$(hostname -f)
 
-echo "Host Information:"
-#hostnamectl command to display OS info
-hostnamectl
+#hostnamectl command to display OS info with awk command to only get the required data
+#FNR == 7 is used for selecting the line 7 and print $3, $4, $5 is used to print the 3rd, 4th, and 5th item field
+hostname=$(hostnamectl | awk 'FNR == 7 {print $3, $4, $5}')
 
-echo "Ip Addresses:"
 #hostname command with uppercase "-I" option to display IP address
-hostname -I
+ip=$(hostname -I)
 
-echo "Root space status:"
-# df command with '/' for root directory and "-h" option for human readable format
-df / -h
+# df command with '/' for root directory and "-h" option for human readable format with awk command to get only the required data
+#FNR == 2 is used for choosing the line 2 and print $4 prints the 4th item field
+freespace=$(df / -h | awk 'FNR == 2 {print $4}')
+
+
+cat <<EOF
+
+Report
+========================================
+FQDN: $fqdn
+Operating System name and version: $hostname
+IP Address: $ip
+Root Filesystem Free Space: $freespace
+========================================
+
+EOF
