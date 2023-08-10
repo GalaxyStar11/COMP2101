2c9ac524765e9afe107905b20715c1d7b62ece2c
GalaxyStar11 <shub200213@gmail.com>
2023-08-10 17:49:06 -0400

    Lab 3

---
+++ Powershell/test sciprt.ps1
@@ -0,0 +1,17 @@
+﻿
+#Ip configuration Report:
+
+$adapters = Get-CimInstance win32_networkadapterconfiguration
+$filteredAdapters = $adapters | Where-Object { $_.IPEnabled -eq $TRUE }
+$myNetworkObjects = $filteredAdapters |
+                            foreach { $adapter=$_;
+                                        New-Object PSObject -Property @{Name=$adapter.servicename;
+                                                                        "Adapter Description"=$adapter.Description;
+                                                                        index=$adapter.index;
+                                                                        IPAddress=$adapter.ipaddress;
+                                                                        "Subnet Mask"=$adapter.ipsubnet;
+                                                                        "DNS Domain Name"=$adapter.dnsdomain;
+                                                                        "DNS Server"=$adapter.dnsserversearchorder
+                                                                        }
+                                        }
+$myNetworkObjects | format-table -autosize Name, "Adapter Description", index, IPAddress, "Subnet Mask", "DNS Domain Name", "DNS Server"
